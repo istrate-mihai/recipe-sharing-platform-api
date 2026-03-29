603a092e0a44903fd264330e8177248556394a05
istrate-mihai <i.mihai9960@gmail.com>
2026-03-29 19:05:29 +0300

    feat: finished platform MVP + removed useless BelongsTo relationship from imports

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
